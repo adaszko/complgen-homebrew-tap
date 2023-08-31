120b217825196e383329b0b80fbe1d9e1265373c
Adam Szkoda <adaszko@gmail.com>
2023-08-31 19:17:31 +0200

    Build from Rust source

--- Formula/complgen.rb
+++ Formula/complgen.rb
@@ -1,15 +1,15 @@
 class Complgen < Formula
-  version "0.0.1"
+  version "0.1.0"
   desc "complgen"
   homepage "https://github.com/adaszko/complgen"
+  url "https://github.com/adaszko/complgen/archive/v#{version}.tar.gz"
+  sha256 "f782255e1d79a48c69ccf0772894c23bb524c40cfa3fa2c4a97484d31f0074d4"
 
-  url "https://github.com/adaszko/complgen/releases/download/v#{version}/complgen-.tar.gz"
-  sha256 "???"
-
+  depends_on "rust" => :build
   depends_on "bash-completion"
 
   def install
-    bin.install "complgen"
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
