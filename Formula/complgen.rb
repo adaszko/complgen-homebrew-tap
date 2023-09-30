5b0a467b46d9f6bb15a0c89e5c2ef1281d05a687
Adam Szkoda <adaszko@gmail.com>
2023-09-30 10:16:35 +0200

    Install completions for complgen itself

--- Formula/complgen.rb
+++ Formula/complgen.rb
@@ -1,15 +1,19 @@
 class Complgen < Formula
-  version "0.1.3"
+  version "0.1.4"
   desc "complgen"
   homepage "https://github.com/adaszko/complgen"
-  url "https://github.com/adaszko/complgen/archive/v#{version}.tar.gz"
-  sha256 "5ec2928d094da0a597e3644e7d2c4d6affdde36ac10e807aa7b8b3c61f4ee0b5"
+  url "https://github.com/adaszko/complgen/archive/v0.1.4.tar.gz"
+  sha256 "9db37ff7b7baacf326dfce35d209aaa99623b0935d874c3f5ab94fa61784c2e9"
 
   depends_on "rust" => :build
   depends_on "bash-completion"
 
   def install
     system "cargo", "install", "--no-track", "--root", prefix, "--path", "."
+    system "cargo", "run", "--release", "--", "compile", "--bash-script", "complgen.bash", "--fish-script", "complgen.fish", "--zsh-script", "complgen.zsh", "usage/complgen.usage"
+    bash_completion.install "complgen.bash"
+    fish_completion.install "complgen.fish"
+    zsh_completion.install "complgen.zsh"
   end
 
   test do
