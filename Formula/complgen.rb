2cdcab98f32979ae73b7893516a7aa2869fcec4c
Adam Szkoda <adaszko@gmail.com>
2023-12-03 15:36:49 +0100

    Bump version

--- Formula/complgen.rb
+++ Formula/complgen.rb
@@ -1,16 +1,16 @@
 class Complgen < Formula
-  version "0.1.7"
+  version "0.1.8"
   desc "complgen"
   homepage "https://github.com/adaszko/complgen"
   url "https://github.com/adaszko/complgen/archive/v#{version}.tar.gz"
-  sha256 "fb0d547e84e63d49e34d6f9edc8e3d495b7886a33da8e9453f77d8af7bb0d9c2"
+  sha256 "1f30f6ff09f188bd186129c09873a554b6ee635aaeaab84bd3827c7a9c2aaecb"
 
   depends_on "rust" => :build
   depends_on "bash-completion"
 
   def install
     system "cargo", "install", "--no-track", "--root", prefix, "--path", "."
-    system "cargo", "run", "--release", "--", "compile", "--bash-script", "complgen.bash", "--fish-script", "complgen.fish", "--zsh-script", "complgen.zsh", "usage/complgen.usage"
+    system "cargo", "run", "--release", "--", "aot", "--bash-script", "complgen.bash", "--fish-script", "complgen.fish", "--zsh-script", "complgen.zsh", "usage/complgen.usage"
     bash_completion.install "complgen.bash"
     fish_completion.install "complgen.fish"
     zsh_completion.install "complgen.zsh"
